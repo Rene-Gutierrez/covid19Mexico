a65bb1cabe292085288b65395719946c76d06f69
unknown <rgutie17@ucsc.edu>
2020-05-09 21:54:15 -0700

    Model for Suspects

--- dataRetrieval.R
+++ dataRetrieval.R
@@ -1,23 +1,6 @@
 ### Daily Data Retrival
 
 # Mexican Data
-## Builds the File Name
-### Gets Today's Date
-datTod <- Sys.Date()
-### Gets the Year
-yea <- sub(paste('.+(?=.{', 2, '})', sep=''), '', year(datTod), perl=T)
-### Gets the Month
-mon <- month(datTod)
-if(mon < 10){
-  mon <- paste0("0",mon)
-}
-### Gets the Day of the Month
-dayMon <- mday(datTod)
-if(dayMon < 10){
-  dayMon <- paste0("0", dayMon)
-}
-### Builds the CSV file name
-filNam <- paste0('./tem2/',yea, mon, dayMon, 'COVID19MEXICO.csv')
 
 ## Downloads the Data
 ### URL
